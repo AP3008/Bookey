89d9604425298d573d5dfe2b320d424fc0895d63
Adam P <adam.porbanderwalla@gmail.com>
2026-02-06 23:04:13 -0500

    feat: started with google auth, mostly got the login functionallity working, just need to complete google cloud console permissions, so I can test if we are able to write the token.json file and then if we can use persistent login.

---
+++ src/bookey/auth.py
@@ -0,0 +1,23 @@
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If you change these scopes, you must delete token.json and log in again.
+
+SCOPES = [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/tasks'
+]
+
+def login():
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_files("token.json", SCOPES)
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+        with open("token.json") as token_json:
+            token_json.write(creds.to_json())
+
+if __name__ == "__main__":
+    login()
