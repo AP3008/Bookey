38c224d93a98344df726c568488582a1cbb21d69
Adam P <adam.porbanderwalla@gmail.com>
2026-02-09 17:52:59 -0500

    feat: stopped using relative pathis in google_calendar.py, so you can run using pipx

--- src/bookey/auth.py
+++ src/bookey/auth.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -12,22 +12,28 @@ SCOPES = [
     'https://www.googleapis.com/auth/tasks'
 ]
 
+CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".config", "bookey")
+TOKEN_PATH = os.path.join(CONFIG_DIR, "token.json")
+CREDS_PATH = os.path.join(CONFIG_DIR, "credentials.json")
+
 def login():
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+
+    if os.path.exists(TOKEN_PATH):
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
 
     # If the user has valid credentials and they are logging in again, we should just refresh the creds, and update our existing json with the new creds
 
-        creds.refresh(Request()) 
-        with open("token.json", "w") as token:
+        creds.refresh(Request())
+        with open(TOKEN_PATH, "w") as token:
             token.write(creds.to_json())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        flow = InstalledAppFlow.from_client_secrets_file(CREDS_PATH, SCOPES)
         creds = flow.run_local_server(port=0)
-    
-    # When the user is logging in through google, we need to write a token.json file so we can log them in without asking again. 
 
-        with open("token.json", "w") as token:
+    # When the user is logging in through google, we need to write a token.json file so we can log them in without asking again.
+
+        with open(TOKEN_PATH, "w") as token:
             token.write(creds.to_json())
 
     return creds
