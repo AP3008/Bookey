81778cc107233b9e5327e04f85eef2a9b0c17873
Adam P <adam.porbanderwalla@gmail.com>
2026-02-08 21:14:49 -0500

    feat: added app.py and added imports that I need to start the TUI design using CSS styling

---
+++ src/bookey/app.py
@@ -0,0 +1,4 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static, Placeholder
+from textual.containers import Container, Horizontal, Vertical
+from bookey.google_calendar import GoogleCalendar
